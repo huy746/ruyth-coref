d0b6304f733fb65ce7174686b5a9e879aa357eeb
Huy <218056340+huy746@users.noreply.github.com>
2025-11-23 18:43:43 +0700

    Update __init__.py

--- ruythcoref/__init__.py
+++ ruythcoref/__init__.py
@@ -1,1 +1,7 @@
+from .advanced_file_converter import AdvancedFileConverter
 
+def convert(src_path, dest_path):
+    """Hàm tiện lợi để chuyển đổi file"""
+    return AdvancedFileConverter.convert(src_path, dest_path)
+
+__all__ = ["convert", "AdvancedFileConverter"]
